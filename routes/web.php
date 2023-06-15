4407bc6f86b4f2b213ef7ef82a7ffa8d88417438
Omar <omar@Omars-MacBook-Air.local>
2023-06-15 15:38:05 +0100

    controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Models\Admin;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/omar', function () {
-    return view('welcome');
-});
+
+Route::resource('admin',AdminController::class);
 
